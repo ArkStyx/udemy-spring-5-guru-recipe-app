6c0b9edba1971a83db9bbf369ce62494b624c354
ArkStyx <regis.petit@gmail.com>
2022-11-20 19:12:13 +0100

    Ajout des repository dans controller pour verification chargement data.sql

--- src/main/java/guru/springframework/recipe/app/controllers/IndexController.java
+++ src/main/java/guru/springframework/recipe/app/controllers/IndexController.java
@@ -1,14 +1,39 @@
 package guru.springframework.recipe.app.controllers;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import guru.springframework.recipe.app.domain.Category;
+import guru.springframework.recipe.app.domain.UnitOfMeasure;
+import guru.springframework.recipe.app.repositories.CategoryRepository;
+import guru.springframework.recipe.app.repositories.UnitOfMeasureRepository;
 
 @Controller
 public class IndexController {
 
+	private final CategoryRepository categoryRepository;
+	private final UnitOfMeasureRepository unitOfMeasureRepository;
+	
+	public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+		super();
+		this.categoryRepository = categoryRepository;
+		this.unitOfMeasureRepository = unitOfMeasureRepository;
+	}
+
 	@RequestMapping({"", "/", "/index"})
 	public String getIndexPage() {
-		System.out.println("COUCOU");
+		
+		String chosenCategory = "American";
+		String chosenUnitOfMeasure = "Teaspoon";
+		
+		Optional<Category> categoryOptional = categoryRepository.findByDescription(chosenCategory);
+		Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription(chosenUnitOfMeasure);
+		
+		System.out.println("Category ID for " + chosenCategory + " is : " + categoryOptional.get().getId());
+		System.out.println("UnitOfMeasure ID for " + chosenUnitOfMeasure + " is : " + unitOfMeasureOptional.get().getId());
+		
 		return "index";
 	}
 }
--- src/main/java/guru/springframework/recipe/app/repositories/RecipeRepository.java
+++ src/main/java/guru/springframework/recipe/app/repositories/RecipeRepository.java
@@ -1,5 +1,7 @@
 package guru.springframework.recipe.app.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import guru.springframework.recipe.app.domain.Recipe;
