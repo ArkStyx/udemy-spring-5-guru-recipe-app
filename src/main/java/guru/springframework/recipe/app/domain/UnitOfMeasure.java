ca53bf011ed75ea3d411c89671bf3d9d9af3e870
ArkStyx <regis.petit@gmail.com>
2022-11-27 17:56:25 +0100

    Correction sur le nommage des entites pour passer par un nommage table BDD

--- src/main/java/guru/springframework/recipe/app/domain/UnitOfMeasure.java
+++ src/main/java/guru/springframework/recipe/app/domain/UnitOfMeasure.java
@@ -4,13 +4,15 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-@Entity(name = "tb_unit_of_measure")
+@Entity
+@Table(name = "tb_unit_of_measure")
 public class UnitOfMeasure {
 
 	@Id
