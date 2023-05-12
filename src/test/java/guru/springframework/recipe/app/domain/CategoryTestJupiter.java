9b77cc3368f98e64aa90a72d757c12f942f95a56
ArkStyx <regis.petit@gmail.com>
2023-05-12 23:19:51 +0200

    commit rapide

--- src/test/java/guru/springframework/recipe/app/domain/CategoryTestJupiter.java
+++ src/test/java/guru/springframework/recipe/app/domain/CategoryTestJupiter.java
@@ -1,7 +1,6 @@
 package guru.springframework.recipe.app.domain;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
